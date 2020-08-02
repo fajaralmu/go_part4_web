c821ba320176082f62ff37e00e950dd149417529
Fajar AM <somabangsa@gmail.com>
2020-08-02 18:34:33 +0700

    add time greeting

---
+++ reflections/dateUtil.go
@@ -0,0 +1,20 @@
+package reflections
+
+import "time"
+
+func GetTimeGreeting() string {
+	//var time string = "Morning"
+	currentTime := time.Now()
+
+	var hour int = currentTime.Hour()
+	var timeStr string
+	if hour >= 3 && hour < 11 {
+		timeStr = "Morning"
+	} else if hour >= 11 && hour < 18 {
+		timeStr = "Afternoon"
+	} else {
+		timeStr = "Evening"
+	}
+
+	return timeStr
+}
